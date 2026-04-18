108a592b5e42e339ba47c9f05e8d737128e3e808
Arunkumar Akilan <arunkumarakilanarun@gmail.com>
2026-04-18 19:41:34 +0100

    Add show selection with click event and fetch episodes dynamically using show ID

--- script.js
+++ script.js
@@ -1,26 +1,78 @@
-//=> store data in single location
-const state = { episode:[], searchTerm: "", selectTerm: "" };
-//=> display episode count 
+const state = {
+  shows: [],
+  episodes: [],
+  searchTerm: "",
+  selectTerm: "",
+  selectShow: "",
+  showId: "",
+};
+
+const showEpisode = document.getElementById("show-container");
+
+//=> display episode count
 function displayCount() {
   const rootElem = document.getElementById("display");
-  rootElem.textContent = `Displaying ${filteredEpisodeCards().length}/${state.episode.length} episodes`;
+  rootElem.textContent = `Displaying ${filteredEpisodeCards().length}/${state.episodes.length} episodes`;
+  //rootElem.textContent = `Displaying ${filteredShowCards().length}/${state.shows.length} episodes`;
+}
+function setup() {
+  async function fetchShow() {
+    try {
+      const response = await fetch("https://api.tvmaze.com/shows");
+      if (!response.ok) {
+        throw new Error(`HTTP error! status:${response.status}`);
+      }
+      const shows = await response.json();
+      state.shows = shows;
+      showRender();
+      return shows;
+    } catch (error) {
+      const root = document.getElementById("root");
+      root.innerHTML = `<div style="
+      text-align:center;
+      padding:30px;
+      background:white;
+      border-radius:12px;
+      max-width:400px;
+      margin:40px auto;
+      box-shadow:0 10px 20px rgba(0,0,0,0.1);
+    ">
+      <h2> ❌  Failed to load</h2>
+      <p>Unable To Fetch the Show</p>
+      <button id="retryBtn">🔄 Retry</button>
+      </div>`;
+
+      document.getElementById("retryBtn").addEventListener("click", () => {
+        root.innerHTML = "loading shows.....";
+        setup();
+        showRender();
+      });
+      console.error("Error fetching films:", error);
+    }
+  }
+
+  fetchShow();
 }
 //=> fetch API
- async function fetchEpisode() {
-     try
-     {
-      const response = await fetch("https://api.tvmaze.com/shows/82/episodes")
-      if(!response.ok)
-      {
-        throw new Error (`HTTP error! status:${response.status}`);
+
+function episodeSource() {
+  async function fetchEpisode() {
+    try {
+      const response = await fetch(
+        `https://api.tvmaze.com/shows/${state.showId}/episodes`,
+      );
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status:${response.status}`);
       }
       const episodes = await response.json();
+      state.episodes = episodes;
+      render();
       return episodes;
-     }
-     catch(error){
-         const root = document.getElementById("root");
+    } catch (error) {
+      const root = document.getElementById("root");
 
-         root.innerHTML = `
+      root.innerHTML = `
     <div style="
       text-align:center;
       padding:30px;
@@ -36,46 +88,72 @@ function displayCount() {
     </div>
   `;
 
-         // 👉 retry logic
-         document.getElementById("retryBtn").addEventListener("click", () => {
-           root.textContent = "Loading episodes...";
-           fetchEpisode().then((episodes) => {
-             if (!episodes) return;
-             state.episode = episodes;
-             render();
-           });
-         });
-      console.error("Error fetching films:",error);
-     }
- };
- fetchEpisode().then((episodes)=>{
-  if(!episodes)return;
-  state.episode = episodes;
-  render();
- })
+      // 👉 retry logic
+      document.getElementById("retryBtn").addEventListener("click", () => {
+        root.textContent = "Loading episodes...";
+        fetchEpisode().then((episodes) => {
+          if (!episodes) return;
+          state.episodes = episodes;
+          render();
+        });
+      });
+      console.error("Error fetching films:", error);
+    }
+  }
+  fetchEpisode();
+}
+//=>create show card for one show
+function createShowCard(show) {
+  const showCard = document.getElementById("film-card").content.cloneNode(true);
+  const cardElement = showCard.querySelector(".card");
+  cardElement.querySelector("h3").textContent = show.name;
+  cardElement.querySelector("img").src = show.image.medium;
+  cardElement.querySelector("p").innerHTML = show.summary;
+  cardElement.addEventListener("click", () => {
+    showEpisode.style.display = "none";
+    state.showId = show.id;
+    episodeSource();
+  });
+  return cardElement;
+}
 
-//=> create film card for one episode 
+//=> create episode card for one episode
 function createEpisodeCard(episode) {
-  const episodeCard = document.getElementById("film-card").content.cloneNode(true);
+  const episodeCard = document
+    .getElementById("film-card")
+    .content.cloneNode(true);
   episodeCard.querySelector("h3").textContent =
     `${episode.name}S${episode.season.toString().padStart(2, "0")}E${episode.number.toString().padStart(2, "0")}`;
   episodeCard.querySelector("img").src = episode.image.medium;
   episodeCard.querySelector("p").innerHTML = episode.summary;
   return episodeCard;
 }
+//=>
+function filteredShowCards() {
+  filteredShow = state.shows.filter((show) => {
+    return show.name === state.selectShow || state.selectShow === "";
+  });
+  return filteredShow;
+}
 //=> filter function for search and select
 function filteredEpisodeCards() {
-  return state.episode.filter((episode) => {
-    const code = `${episode.name}S${episode.season.toString().padStart(2, "0")}E${episode.number.toString().padStart(2, "0")}`;
+  return state.episodes.filter((episodes) => {
+    const code = `${episodes.name}S${episodes.season.toString().padStart(2, "0")}E${episodes.number.toString().padStart(2, "0")}`;
 
     const searchInputMatch =
       code.toLowerCase().includes(state.searchTerm.toLowerCase()) ||
-      episode.summary.toLowerCase().includes(state.searchTerm.toLowerCase());
+      episodes.summary.toLowerCase().includes(state.searchTerm.toLowerCase());
     const selectInputMatch =
-      `S${episode.season.toString().padStart(2, "0")}E${episode.number.toString().padStart(2, "0")}-${episode.name}` ===
+      `S${episodes.season.toString().padStart(2, "0")}E${episodes.number.toString().padStart(2, "0")}-${episodes.name}` ===
         state.selectTerm || state.selectTerm === "";
     return searchInputMatch && selectInputMatch;
   });
+}
+//=> Create show list for select drop down
+function createShowList(show) {
+  const showList = document.createElement("option");
+  showList.textContent = show.name;
+  return showList;
 }
 //=> create episode list for select drop down
 function createEpisodeList(episode) {
@@ -83,16 +161,44 @@ function createEpisodeList(episode) {
   episodeList.textContent = `S${episode.season.toString().padStart(2, "0")}E${episode.number.toString().padStart(2, "0")}-${episode.name}`;
   return episodeList;
 }
+//=> render function for render the show
+function showRender() {
+  const showList = state.shows
+    .sort(function (a, b) {
+      if (a.name < b.name) {
+        return -1;
+      }
+      if (a.name > b.name) {
+        return 1;
+      }
+      return 0;
+    })
+    .map(createShowList);
+  document.getElementById("show-list").append(...showList);
+  const showCard = filteredShowCards()
+    .sort(function (a, b) {
+      if (a.name < b.name) {
+        return -1;
+      }
+      if (a.name > b.name) {
+        return 1;
+      }
+      return 0;
+    })
+    .map(createShowCard);
+  document.getElementById("show-container").innerHTML = "";
+  document.getElementById("show-container").append(...showCard);
+  displayCount(state.shows);
+}
 //=> render function for render the UI
 function render() {
-  const episodeList = state.episode.map(createEpisodeList);
+  const episodeList = state.episodes.map(createEpisodeList);
   document.getElementById("select").append(...episodeList);
   const episodeCard = filteredEpisodeCards().map(createEpisodeCard);
   document.getElementById("root").innerHTML = "";
   document.getElementById("root").append(...episodeCard);
-  displayCount(state.episode);
+  displayCount(state.episodes);
 }
-render();
 //=> event listener for select and search
 const input = document.getElementById("search");
 input.addEventListener("keyup", () => {
@@ -105,3 +211,9 @@ select.addEventListener("change", () => {
   state.selectTerm = select.value;
   render();
 });
+const selectShow = document.getElementById("show-list");
+selectShow.addEventListener("change", () => {
+  state.selectShow = selectShow.value;
+  showRender();
+});
+window.onload = setup;
